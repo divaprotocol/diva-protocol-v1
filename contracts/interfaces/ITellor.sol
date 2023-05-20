f2be3292d1aa0429248b1770be8961fb41ee723a
Walodja1987 <wladimir.weinbender@gmail.com>
2023-05-20 09:26:50 +0200

    Missing NatSpec in `ITellor.sol` added

--- contracts/interfaces/ITellor.sol
+++ contracts/interfaces/ITellor.sol
@@ -2,6 +2,14 @@
 pragma solidity 0.8.19;
 
 interface ITellor {
+    /**
+     * @dev Retrieves the latest value for the queryId before the specified timestamp
+     * @param _queryId is the queryId to look up the value for
+     * @param _timestamp before which to search for latest value
+     * @return _ifRetrieve bool true if able to retrieve a non-zero value
+     * @return _value the value retrieved
+     * @return _timestampRetrieved the value's timestamp
+     */
     function getDataBefore(bytes32 _queryId, uint256 _timestamp)
         external
         view
